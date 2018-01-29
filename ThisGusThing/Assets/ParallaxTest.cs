b178f7d83942c3410bc04e239c048b75ec1f9950
HanniBun <johannanas.nilsson@gmail.com>
2018-01-29 17:12:30 +0100

    Parallax test
    Lade in tre bakgrunder som med ett script rör sig i olika hastigheter när kameran rörs. Borde fungera OK! Går att lägga in hur många bakgrunder som helst ;)

---
+++ ThisGusThing/Assets/ParallaxTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxTest : MonoBehaviour {
+
+    public Transform[] backgrounds;
+    private float[] parallaxScales; // The proportion of the camera's movement to move the backgrounds by. :)
+    public float smoothness = 1f; // Makes things smoooooth. Always above 0!
+
+    private Transform myCamera;
+    private Vector3 previousCameraPos;
+
+
+    private void Awake() 
+    {
+        myCamera = Camera.main.transform;
+    }
+
+    void Start() {
+        previousCameraPos = myCamera.position;
+
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1; // Brackeys made me do it.
+        }
+	}
+
+    void Update() {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float parallax = (previousCameraPos.x - myCamera.position.x) * parallaxScales[i];
+
+            float backgroundTargetPositionx = backgrounds[i].position.x + parallax; // The target position for the background[i].
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPositionx, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            //time to fade shit (current and target pos) with lerp
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothness * Time.deltaTime);                  
+        }
+
+        previousCameraPos = myCamera.position;
+        
+	}
+}
