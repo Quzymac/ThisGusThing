85cfd95bcd7b71e3d52c70ba3975d3e1db1a7d78
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-01-30 16:00:11 +0100

    whiteknight

---
+++ ThisGusThing/Assets/Scripts/WhiteKnight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteKnight : MonoBehaviour {
+
+    [SerializeField] float MoveSpeed = 5f;
+
+	void Update () {
+        transform.Translate(Time.deltaTime * MoveSpeed ,  0 , 0);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Rotate();
+        }
+    }
+    void Rotate()
+    {
+        transform.Rotate(0, 180, 0);
+    }
+}
