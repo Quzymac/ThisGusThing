7b12cc4955172b954b85ae9485a53e64ca94b2b4
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-01-31 15:59:16 +0100

    mutationManager

---
+++ ThisGusThing/Assets/Scripts/MutationManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MutationManager : MonoBehaviour {
+
+    [SerializeField] int mutationCurrency = 1;
+
+    [Header("Tier1Ability")]
+    [SerializeField] bool doubleJump = false;
+
+    [Header("Tier2Ability")]
+    [SerializeField] bool trippleJump = false;
+    [SerializeField] bool superSpeed = false;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Tier1
+    public void ActivateDoubleJump()
+    {
+        if(mutationCurrency > 0 && (!trippleJump || !superSpeed))
+        {
+            doubleJump = true;
+            mutationCurrency--;
+        }
+    }
+
+    //Tier2
+    public void ActivateTrippleJump()
+    {
+        if (mutationCurrency > 0 && doubleJump && !superSpeed) // && check for tier3
+        {
+            trippleJump = true;
+            mutationCurrency--;
+        }
+    }
+
+    public void ActivateSuperSpeed()
+    {
+        if (mutationCurrency > 0 && doubleJump && !trippleJump) // && check for tier3
+        {
+            superSpeed = true;
+            mutationCurrency--;
+        }
+    }
+
+    public void PickUpMutationCurrency()
+    {
+        mutationCurrency++;
+    }
+
+}
