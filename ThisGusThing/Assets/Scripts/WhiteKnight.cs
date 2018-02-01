5047d8318c063eab3660b6adceb17de0c8ca2f73
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-02-01 11:27:43 +0100

    whiteknight raycast och rotation

--- ThisGusThing/Assets/Scripts/WhiteKnight.cs
+++ ThisGusThing/Assets/Scripts/WhiteKnight.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 
 public class WhiteKnight : MonoBehaviour {
 
-    [SerializeField] float MoveSpeed = 5f;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float raycastLength;
+    bool hitObject = false;
 
-    void Update () {
-        transform.Translate(Time.deltaTime * MoveSpeed ,  0 , 0);
-	}
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] GameObject graficsModel;
+    float rotationTime;
+    Quaternion targetRotation;
+    [SerializeField] float rotationSpeed = 1f;
+    bool rotating = false;
+    bool facingRight = true;
+    
+    void Update()
     {
-         Rotate();
-    }
-    void Rotate()
-    {
-        transform.Rotate(0, 180, 0);
+        if (facingRight)
+        {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            if (hitObject == false && Physics.Raycast(transform.position, right, raycastLength))
+            {
+                hitObject = false;
+                targetRotation = Quaternion.Euler(0, 270, 0);
+                facingRight = false;
+                rotating = true;
+                rotationTime = 0;
+            }
+        }
+        else if (!facingRight)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+            Vector3 left = transform.TransformDirection(Vector3.left);
+            if (hitObject == false && Physics.Raycast(transform.position, left, raycastLength))
+            {
+                hitObject = false;
+                targetRotation = Quaternion.Euler(0, 90, 0);
+                facingRight = true;
+                rotating = true;
+                rotationTime = 0;
+            }
+        }
+
+        if (rotating)
+        {
+            
+            rotationTime += Time.deltaTime * rotationSpeed;
+            graficsModel.transform.rotation = Quaternion.Lerp(graficsModel.transform.rotation, targetRotation, rotationTime);
+            if (rotationTime > 1)
+            {
+                rotating = false;
+            }
+        }
     }
 }
