50c1ad6f814e21aebf21ecee140e47f0b9ba15a4
Quzymac <33623813+Quzymac@users.noreply.github.com>
2018-02-08 12:56:03 +0100

    grafik | markfix

---
+++ ThisGusThing/Assets/Scripts/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+    [SerializeField] AudioClip victorySound;
+    bool win = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!win)
+        {
+            StartCoroutine(Win());
+            win = true;
+        }
+    }
+    
+    IEnumerator Win()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(victorySound);
+        yield return new WaitForSeconds(victorySound.length);
+        print("f");
+        SceneManager.LoadScene("EndCredits");
+
+    }
+}
